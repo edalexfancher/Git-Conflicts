6e01edc62238e2f052e4b46bf326df9c85198035
Cora Quezada <corafenice1013@gmail.com>
2018-03-21 10:36:34 -0400

    added feature

--- engine.rb
+++ engine.rb
@@ -1,3 +1,21 @@
 class GameEngine
-  puts 'pretend this is a feature'
+  words = ["cat", "elephant", "turtle", "bird", "snake"]
+
+  picked = words.sample
+  options = ["yes", "no"]
+
+
+  puts "-"*60
+  play = puts "Are you ready to guess?"
+  puts options.join("....or....")
+
+  user_input = gets.chomp
+
+  if user_input == "yes"
+    puts "I'm thinking of an animal. It's a #{picked.length} letter word.
+    Can you guess what it it?"
+  elsif user_input == "no"
+    puts "It's ok.....I'll be here.Try me later"
+    exit
+  end
 end
