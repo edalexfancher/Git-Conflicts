6101b0d98a505ccf8cf8670b52a5371c2b8e9005
Eddie Alex <edmlounge@gmail.com>
2018-03-21 10:37:50 -0400

    Added cora loop

--- engine.rb
+++ engine.rb
@@ -1,2 +1,8 @@
 class GameEngine
+  @@serialnum = 1
+
+  def initialize
+    @serialnum = @@serialnum
+    @@serialnum += 1
+  end
 end
--- game.rb
+++ game.rb
@@ -3,3 +3,8 @@ require './engine.rb'
 puts 'Welcome to Wyncodiam'
 puts 'Enter your name:'
 user_name = gets.chomp
+
+2000.times do |x|
+  puts 'Hi Cora' + 'a' * x
+  sleep 0.1
+end
